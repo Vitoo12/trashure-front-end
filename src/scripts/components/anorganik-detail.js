979b272d0847461266b5d9646cf3e908e57b8dc9
Alifia Putri Q <71966318+alifiapq@users.noreply.github.com>
2022-12-04 03:07:32 +0700

    Penambahan fitur tips page dan detail pagenya

--- src/scripts/components/anorganik-detail.js
+++ src/scripts/components/anorganik-detail.js
@@ -1,17 +1,74 @@
-import './skeleton/skeleton-detail';
+import "./skeleton/skeleton-detail";
+import UrlParser from "../routes/url-parser";
 
 class AnorganikDetail extends HTMLElement {
-  connectedCallback() {
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  async connectedCallback() {
     this.render();
+    const { id } = UrlParser.parseActiveUrlWithoutCombiner();
+    const reqAnorganik = await fetch(
+      `https://trashure-back-end-production.up.railway.app/anorganik/${id}`,
+      {
+        method: "GET",
+      }
+    );
+    const reqAnorganikJson = await reqAnorganik.json();
+    const dataFromApi = reqAnorganikJson.data;
+    this.data = dataFromApi;
+    this.htmlstring = this.template(this.data);
+    this.firstElementChild.innerHTML = this.htmlstring;
+    this.htmlstringStep = this.createhtmlListStepAnorganik(this.data.Tips);
+    this.stepAnorganikElement = this.querySelector(".anorganik-step");
+    this.stepAnorganikElement.innerHTML = this.htmlstringStep;
+  }
+
+  template(anorganik) {
+    const { name, description, image, video } = anorganik;
+
+    return `
+    <div class="anorganik-name">
+    <h3 class="card-title py-2">${name.toUpperCase()}</h3>
+    <p class="video-text">Video = <a href="${video}" class="link-success">${video}</a> </p> 
+    <img src="${image}" class="img-anorganik-detail" alt="..." />
+    <p class="video-text">${description} </p>
+    </div>
+    <div class="anorganik-step"></div>
+    `;
+  }
+
+  createhtmlStepAnorganik(stepAnorganik, index) {
+    const { title, descDetail, imageDetail } = stepAnorganik;
+    return `
+    <h5 class="card-title py-2">${index + 1}. ${title
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ")}</h5>
+    <img src="${imageDetail}" class="img-anorganik-detail" alt="..." />
+    <p class="text-break" >${descDetail}</p>
+    `;
+  }
+
+  createhtmlListStepAnorganik(arrayAnorganik) {
+    const array = [];
+    for (let i = 0; i < arrayAnorganik.length; i++) {
+      const objectA = arrayAnorganik[i];
+      const objectAnorganik = this.createhtmlStepAnorganik(objectA, i);
+      array.push(objectAnorganik);
+    }
+    return array.join("");
   }
 
   async render() {
     this.innerHTML = `
-        <div class="container-lg">
+        <div class="container-lg" >
           <skeleton-detail></skeleton-detail>
         </div>
                 `;
   }
 }
 
-customElements.define('anorganik-detail', AnorganikDetail);
+customElements.define("anorganik-detail", AnorganikDetail);
--- src/scripts/components/anorganik-page.js
+++ src/scripts/components/anorganik-page.js
@@ -1,24 +1,101 @@
-import './skeleton/skeleton-tips';
+import "./skeleton/skeleton-tips";
 
 class AnorganikPage extends HTMLElement {
-  connectedCallback() {
+  constructor() {
+    super();
+    this.filter;
+    this.data = [];
+  }
+
+  async connectedCallback() {
     this.render();
+
+    const reqAnorganik = await fetch(
+      "https://trashure-back-end-production.up.railway.app/anorganik",
+      {
+        method: "GET",
+      }
+    );
+    const reqAnorganikJson = await reqAnorganik.json();
+    const dataFromApi = reqAnorganikJson.data;
+    this.data.push(...dataFromApi);
+    this.htmlstring = this.createhtmlListAnorganik(this.data);
+    this.searchButton = this.querySelector("button.search");
+    this.searchInput = this.querySelector(".search-input");
+    this.listAnorganikElement = this.querySelector(".container-anorganik");
+    this.listAnorganikElement.classList.add("row");
+    this.listAnorganikElement.innerHTML = this.htmlstring;
+    this.searchListener();
+  }
+
+  searchListener() {
+    this.searchButton.addEventListener("click", () => {
+      const inputText = this.searchInput.value;
+      this.filter = this.data.filter((anorganik) =>
+        anorganik.name.toLowerCase().includes(inputText.toLowerCase())
+      );
+      this.listAnorganikElement.innerHTML = this.createhtmlListAnorganik(
+        this.filter
+      );
+    });
+  }
+
+  createHtmlAnorganik(anorganik) {
+    const { name, description, image, id } = anorganik;
+    return `
+      <div class="col-xs-12 col-sm-6 col-lg-4 col-xl-3 my-3">
+        <div class="card">
+          <img src="${image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-page card-text text-break">${description}</p>
+            <a href="https://trashure-back-end-production.up.railway.app/anorganik/${id}" type="button" class="btn btn-warning">Detail >></a>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createhtmlListAnorganik(arrayAnorganik) {
+    const array = [];
+    for (let i = 0; i < arrayAnorganik.length; i++) {
+      const objectA = arrayAnorganik[i];
+      const objectAnorganik = this.createHtmlAnorganik(objectA);
+      array.push(objectAnorganik);
+    }
+    return array.join("");
   }
 
   async render() {
     this.innerHTML = `
-    <div class="container-expand-lg">
-    <h1 class="text-center mt-4">Tips Anorganik</h1>
-    <div class="container-expand-lg bg-success p-4 rounded-5 rounded-bottom">
-      <div class="d-flex justify-content-center">
-        <input type="text" class="rounded w-100 px-2" style="height:44px" placeholder="Search Tips">
+      <div class="container-expand-lg">
+        <h1 class="text-center mt-4">Tips Anorganik</h1>
+        <div
+          class="container-expand-lg bg-success p-4 rounded-5 rounded-bottom"
+        >
+          <div class="d-flex justify-content-center">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="search-input form-control"
+                placeholder="Mau daur ulang limbah apa hari ini?"
+                aria-label="Mau daur ulang limbah apa hari ini?"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append ms-1">
+                <button class="search form-control bg-warning">
+                  <i class="bi bi-search"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="container-expand-lg container-anorganik">
+            <skeleton-tips class="row"></skeleton-tips>
+          </div>
+        </div>
       </div>
-      <div class="container-expand-lg container-anorganik">
-        <skeleton-tips class="row"></skeleton-tips>
-      </div>
-    </div>
-              `;
+    `;
   }
 }
 
-customElements.define('anorganik-page', AnorganikPage);
+customElements.define("anorganik-page", AnorganikPage);
